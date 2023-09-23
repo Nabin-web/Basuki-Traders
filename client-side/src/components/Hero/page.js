da426b884c69a6c1cab170814f32917b51aa4444
Nabin-web <nabin.kutu11@gmail.com>
2023-09-23 20:31:17 +0545

    updates on hero section

--- client-side/src/components/Hero/page.js
+++ client-side/src/components/Hero/page.js
@@ -3,30 +3,25 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <section className="h-screen grid grid-cols-1  sm:grid-cols-2 mt-20 mx-auto">
-      <div className="text-center mx-auto pt-16">
-        <h1 className=" text-2xl sm:text-6xl font-semibold tracking-wider">
-          Welcome to <br />
-          <span className=" text-orange-500 ">Ashirbad Traders !</span>
+    <section className="h-screen flex items-center gap-20 mt-10 mx-auto">
+      <Image
+        src="/herosection.png"
+        className="ml-16"
+        priority
+        height={450}
+        width={380}
+        quality={85}
+      />
+      <div>
+        <h1 className="text-5xl font-bold mb-3">
+          All your{" "}
+          <span className="text-orange-500 capitalize">grocery needs</span>{" "}
         </h1>
-        <p className="font-light text-xl sm:text-3xl mt-5 tracking-wide">
-          One stop shop for you <br /> all you{" "}
-          <span className=" text-orange-500">Grocery Needs</span>
+        <h1 className="text-5xl font-bold mb-4">under one roof.</h1>
+        <p className="text-gray-500">
+          We believe in making your shopping experience as convenient as
+          possible.
         </p>
-        <a
-          className="px-5 py-2 inline-block border-2 border-orange-500 hover:bg-orange-400 transition-colors mt-10"
-          href=""
-        >
-          Get Started
-        </a>
-      </div>
-      <div className="relative w-[600px] h-[400px]">
-        <Image
-          src="/herosection.jpg"
-          className=" w-full h-full"
-          fill
-          quality={85}
-        />
       </div>
     </section>
   );
