55e6d173a67390b89e2ee74fee2d6fc998bb94cc
Nabin-web <nabin.kutu11@gmail.com>
2023-09-23 19:50:23 +0545

    Updates

--- client-side/src/components/BlurImage/index.js
+++ client-side/src/components/BlurImage/index.js
@@ -5,23 +5,22 @@ export default function BlurImage({ image }) {
   const [isLoading, setLoading] = useState(true);
 
   return (
-    <a href={image.href} className="group">
-      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-        <Image
-          alt="Product Image"
-          src={image}
-          layout="fill"
-          objectFit="contain"
-          className={`
-              duration-700 ease-in-out group-hover:opacity-75
+    // <Link prefetch={false} href={image.href} className="group">
+    <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+      <Image
+        alt="Product Image"
+        src={image}
+        layout="fill"
+        className={`
+              duration-700 ease-in-out group-hover:opacity-75 object-contain
               ${
                 isLoading
                   ? "scale-110 blur-2xl grayscale"
                   : "scale-100 blur-0 grayscale-0"
               })`}
-          onLoadingComplete={() => setLoading(false)}
-        />
-      </div>
-    </a>
+        onLoadingComplete={() => setLoading(false)}
+      />
+    </div>
+    // </Link>
   );
 }
--- client-side/src/containers/Product/Popular/page.js
+++ client-side/src/containers/Product/Popular/page.js
@@ -1,45 +1,60 @@
 "use client";
-import Image from "next/image";
-import React, { useState } from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
-import { BASE_URL, IMAGE_URL, fetcher, options } from "@/utils/Api";
-import useSWR from "swr";
 import BlurImage from "@/components/BlurImage";
+import { BASE_URL, IMAGE_URL, fetcher, options } from "@/utils/Api";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { BsChevronRight } from "react-icons/bs";
+import { RiArrowLeftLine, RiArrowRightLine } from "react-icons/ri";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import useSWR from "swr";
 
 // const data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const NextArrow = ({ onClick }) => {
   return (
-    <div className="absolute right-0 -top-[80px]" onClick={onClick}>
-      <div className=" bg-[#fab1a0] h-[50px] w-[50px] rounded-full grid place-items-center cursor-pointer ">
-        <BsChevronRight />
-      </div>
-    </div>
+    <button
+      title="Previous"
+      className="absolute -right-0 top-1/2 z-10 -translate-y-1/2"
+      onClick={onClick}
+    >
+      <RiArrowRightLine className="text-white bg-orange-500 p-2 font-bold text-5xl rounded-full shadow-md opacity-60 hover:opacity-100 duration-200" />
+    </button>
   );
 };
 
 const PrevArrow = ({ onClick }) => {
   return (
-    <div className="absolute right-[80px] -top-[80px]" onClick={onClick}>
-      <div className=" bg-[#fab1a0] h-[50px] w-[50px] rounded-full grid place-items-center cursor-pointer ">
-        <BsChevronLeft />
-      </div>
-    </div>
+    <button
+      title="Next"
+      className="absolute top-1/2 z-10 -translate-y-1/2 -left-5"
+      onClick={onClick}
+    >
+      <RiArrowLeftLine className="text-white bg-orange-500 p-2 font-bold text-5xl rounded-full shadow-md opacity-60 hover:opacity-100 duration-200" />
+    </button>
   );
 };
 
 const PopularProduct = () => {
   const router = useRouter();
   const [progress, setProgress] = useState(0);
-  const { data, mutate, isLoading } = useSWR(
-    { url: `${BASE_URL}product`, headers: options },
+  const {
+    data: popularData,
+    mutate,
+    isLoading,
+  } = useSWR(
+    {
+      url: `${BASE_URL}product/public/list/popular/products`,
+      headers: options,
+    },
     fetcher,
     { revalidateOnFocus: false }
   );
+
+  const data = popularData?.data || [];
+
+  console.log(data);
 
   const [slidesToShow, setSlideToShow] = useState(4);
   var settings = {
@@ -84,28 +99,24 @@ const PopularProduct = () => {
       <h1 className=" text-2xl mb-10">Popular Product</h1>
       <div className=" relative ">
         <Slider {...settings}>
-          {data?.data?.map((each, index) => (
+          {data?.map((each, index) => (
             <div
               className="group transition duration-300 w-[100%] border rounded-lg border-gray-300 hover:text-orange-500  hover:border-orange-500 text-center mb-10 py-8 hover:cursor-pointer"
               key={index}
               onClick={() => handleShowDetails(each.url_key)}
             >
               <div className="relative h-[180px] w-2/3  mx-auto">
-                {/* <Image
-                  src={`${IMAGE_URL}${each.image.path}`}
-                  className="absolute h-full w-full"
-                  fill
-                  alt="Product image"
-                /> */}
                 <BlurImage image={`${IMAGE_URL}${each.image.path}`} />
               </div>
 
-              <h3 className=" font-bold my-4">{each.name}</h3>
+              <h3 className=" font-bold my-4 text-center w-56 mx-auto truncate">
+                {each.name}
+              </h3>
               <p className=" font-bold my-4 text-orange-400">
                 Rs. {each.price}{" "}
               </p>
 
-              <button className=" border border-gray-500 rounded-xl py-2 px-4 flex items-center gap-1 mx-auto group-hover:border-orange-500 ">
+              <button className="flex justify-center w-full items-center gap-2 text-sm mt-8 hover:text-orange-500">
                 Show Details <BsChevronRight />
               </button>
             </div>
