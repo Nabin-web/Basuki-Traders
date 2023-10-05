5f888beb5c1b1c360fef68f320d7baeac9741069
Nabin-web <nabin.kutu11@gmail.com>
2023-10-05 23:28:36 +0545

    Updates on our network

--- client-side/src/utils/constants.js
+++ client-side/src/utils/constants.js
@@ -1,3 +1,24 @@
 export const CURRENCY_SIGN = "Rs.";
 export const INTL_NUMBER_FORMAT = "en-IN"; // en-US
 export const DATE_FORMAT = "YYYY-MM-DD";
+
+export const COUNTRIES = [
+  {
+    name: "India",
+  },
+  {
+    name: "Australia",
+  },
+  {
+    name: "United States",
+  },
+  {
+    name: "United Kingdom",
+  },
+  {
+    name: "Nepal",
+  },
+  {
+    name: "China",
+  },
+];
